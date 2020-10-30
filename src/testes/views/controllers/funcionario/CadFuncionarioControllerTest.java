eba1c970678b7af3c3273458ad1cada2733b0749
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2020-10-30 01:13:04 -0300

    add. src/testes/views/controllers/funcionario

---
+++ src/testes/views/controllers/funcionario/CadFuncionarioControllerTest.java
@@ -0,0 +1,57 @@
+package testes.views.controllers.funcionario;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CadFuncionarioControllerTest {
+    //não sei se vai precisar de teste unitários em todos os botões do CRUD...
+	//testGetCadFuncionario() -> pra verificar o carregamento da view
+	//testInitializeURLResourceBundle() -> pra verificar o initialize
+	
+	@Test
+	void testBtnDemitir_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBtnEditar_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBtnExcluir_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCadFuncionario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBtnGravar_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBtnLimpar_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBtnVoltar_Action() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitializeURLResourceBundle() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/testes/views/controllers/funcionario/PesquisaFuncionarioControllerTest.java
@@ -0,0 +1,16 @@
+package testes.views.controllers.funcionario;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PesquisaFuncionarioControllerTest {
+
+	//deixei apenas instanciado o teste para inicialização da tela.
+	
+	@Test
+	void testGetPesquisaFuncionario() {
+		fail("Jupiter, o quinto planeta do sistema solar. [NOT IMPLEMENTED YET]");
+	}
+
+}
