02903f169dd857e319275a8abda4885b5dca4033
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2020-11-05 18:50:13 -0300

    UPDATE. TESTE FUNCIONARIO

--- src/testes/views/controllers/funcionario/CadFuncionarioControllerTest.java
+++ src/testes/views/controllers/funcionario/CadFuncionarioControllerTest.java
@@ -1,6 +1,8 @@
 package testes.views.controllers.funcionario;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +10,7 @@ import java.time.LocalDate;
 
 import dao.FuncionarioDAO;
 import entitys.Funcionario;
+import javafx.stage.Stage;
 import views.controllers.HomeController;
 import views.controllers.funcionario.CadFuncionarioController;
 import views.controllers.funcionario.PesquisaFuncionarioController;
@@ -46,18 +49,94 @@ class CadFuncionarioControllerTest {
 		func = new Funcionario();
 		funcDAO = new FuncionarioDAO();		
 	}
-		
-	@Test 
-	@DisplayName("Demissao do Funcionario")
-	void testDemitir() {
-		error = "Erro ao tentar executar comando de exclusão do funcionario";
+	
+	@Test
+	@DisplayName("Buscar Funcionario por ID")
+	@RepeatedTest(10)
+	void testBuscarID() {
+		error = "Erro ao tentar buscar fazer a busca do funcionario por ID(int)";
 		try {
-						
-			funcDAO.deletar(setFuncionario().getCod());
-
-			assertEquals(1, setFuncionario().getCod(), error);
-					
 			
+			funcDAO.buscarId(setFuncionario().getCod());
+			assertEquals(1 , setFuncionario().getCod(), error);		
+
+			
+			int funcionarioID = 2;
+			funcDAO.buscarId(funcionarioID);
+			assertNotEquals(funcionarioID , setFuncionario().getCod(), error);	
+			assertEquals(2 , funcionarioID, error);		
+		
+		} catch (Exception e) {
+			fail(error);
+		}
+			
+	}
+
+	
+	@Test
+	@DisplayName("Verificar Documento")
+	void testVerificaRG() {
+		
+		String rg = "123123123";
+		String cpf = "12312312312";
+		
+		funcDAO.verificaRG(rg);
+		assertEquals("123123123", rg);		
+		
+		funcDAO.verificaRG(cpf);
+		assertEquals("12312312312", cpf);
+		
+		funcDAO.verificaRG(setFuncionario().getDocumento());
+		assertNotEquals(123455789, setFuncionario().getDocumento());
+		
+	}
+		
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"1999-03-01", "2005-11-30", "2020-05-01" })
+	@DisplayName("Teste Recebimento de Data")
+	void testConvertData(LocalDate data) {
+		error = "Erro ao tentar converter uma Date para LocalDate";
+		try {			
+				func.setData_contratacao(data);
+				func.setData_demissao(data);
+				
+				assertAll(
+						() -> assertTrue(data != null, error),
+						() -> assertTrue(func.getData_contratacao() != null, error),
+						() -> assertTrue(func.getData_demissao() != null, error)
+						);
+				
+		} catch (Exception e) {
+			fail(error);
+		}
+	}
+	
+	
+	@Test 
+	@DisplayName("Gravacao do Funcionario")
+	//@RepeatedTest(2) //a primeira deve passar, a segunda deve gerar duplicidade de documento no e ser abortada.
+	void testGravar() {
+		error = "Erro ao tentar executar comando de gravação do funcionario";
+		try {
+			
+			funcDAO.inserir(setFuncionario());
+			
+			assertAll(
+					() -> assertEquals(1, setFuncionario().getCod(), error),
+					() -> assertEquals("Nome", setFuncionario().getNome(), error),
+					() -> assertEquals("a@a.com", setFuncionario().getEmail(), error),
+					() -> assertEquals("12312312", setFuncionario().getTelefone(), error),
+					() -> assertEquals("123455789", setFuncionario().getDocumento(), error),
+					() -> assertEquals("rua Ruaruarua", setFuncionario().getEndereco(), error),
+					() -> assertEquals(9999.99, setFuncionario().getSalario(), error),
+					() -> assertEquals(LocalDate.of(2020, 10, 10), setFuncionario().getData_contratacao(), error),
+					() -> assertEquals(null, setFuncionario().getData_demissao(), error),
+					() -> assertEquals(1, setFuncionario().getCod_acesso(), error),
+					() -> assertEquals("1", setFuncionario().getSenha_funcionario(), error)
+					
+					);
+		
 		} catch (Exception e) {
 			fail(error);
 		}
@@ -88,37 +167,23 @@ class CadFuncionarioControllerTest {
 		} catch (Exception e) {
 			fail(error);
 		}
-	}
+	}	
 	
 	@Test 
-	@DisplayName("Gravacao do Funcionario")
-	//@RepeatedTest(2)
-	void testGravar() {
-		error = "Erro ao tentar executar comando de gravação do funcionario";
+	@DisplayName("Demissao do Funcionario")
+	void testDemitir() {
+		error = "Erro ao tentar executar comando de exclusão do funcionario";
 		try {
-			
-			funcDAO.inserir(setFuncionario());
-			
-			assertAll(
-					() -> assertEquals(1, setFuncionario().getCod(), error),
-					() -> assertEquals("Nome", setFuncionario().getNome(), error),
-					() -> assertEquals("a@a.com", setFuncionario().getEmail(), error),
-					() -> assertEquals("12312312", setFuncionario().getTelefone(), error),
-					() -> assertEquals("123455789", setFuncionario().getDocumento(), error),
-					() -> assertEquals("rua Ruaruarua", setFuncionario().getEndereco(), error),
-					() -> assertEquals(9999.99, setFuncionario().getSalario(), error),
-					() -> assertEquals(LocalDate.of(2020, 10, 10), setFuncionario().getData_contratacao(), error),
-					() -> assertEquals(null, setFuncionario().getData_demissao(), error),
-					() -> assertEquals(1, setFuncionario().getCod_acesso(), error),
-					() -> assertEquals("1", setFuncionario().getSenha_funcionario(), error)
+						
+			funcDAO.deletar(setFuncionario().getCod());
+
+			assertEquals(1, setFuncionario().getCod(), error);
 					
-					);
-		
+			
 		} catch (Exception e) {
 			fail(error);
 		}
 	}
-	
 	
 	@Nested
 	@DisplayName("Teste")
